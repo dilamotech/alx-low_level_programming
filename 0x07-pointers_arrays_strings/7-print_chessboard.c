8c97b4546b7f7458ca4842704906099274646456
mygithub-s <ef.dilamo@gmail.com>
2022-09-26 11:26:30 +0000

    somechanges

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,4 @@
-#include "coding.h"
+#include "main.h"
 
 /**
  * print_chessboard - function with 1 argument
@@ -11,10 +11,11 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i <= 7; i++)
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
+		for (j = 0; j <= 7; j++)
+		{	_putchar(a[i][j]);
+		}
+		_putchar(10);
 	}
 }
