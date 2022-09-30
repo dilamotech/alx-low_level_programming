dfed461171ed8bc232d56256311999c734f345d0
mygithub-s <ef.dilamo@gmail.com>
2022-09-30 15:13:42 +0000

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,28 @@
-#include "maqin.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - adds positive numbers
- * @argc: argument count
- * @argv: argument vector for values
- *
- * Return: Always 0 (Success)
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, 1 on failure
  */
+
 int main(int argc, char *argv[])
 {
-	int i, j, sum;
+	int i, j, sum = 0;
 
-	sum = 0;
-
-	for (i = 1; i < argc ; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0' ; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] < 47 || argv[i][j] > 57)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		sum = sum + atoi(argv[i]);
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
