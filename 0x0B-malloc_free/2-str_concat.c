4a9084e87257d544c52e6b53a16d4517673ee48d
mygithub-s <ef.dilamo@gmail.com>
2022-10-03 16:44:50 +0000

    malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include<stdio.h>
 
 /**
  * _len - calculate length of two strings.
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,14 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-int _putchar(char c);
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+char *create_array(unsigned int, char);
+char *_strdup(char *);
+char *str_concat(char *, char *);
+int **alloc_grid(int, int);
 void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
+
+
 #endif
